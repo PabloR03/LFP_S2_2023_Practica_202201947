584342f809c6fe705210d3ba4f6ad85477ed71f6
Pablo Rodríguez Lima <pabloa10rodriguez@gmail.com>
2023-08-10 17:48:06 -0600

    Esqueleto para cargar inventario

---
+++ Practica_202201947/Inventario_DAO.py
@@ -0,0 +1,40 @@
+from inventarioClass import datos_producto
+
+class inventario_DAO:
+    def __init__(self):
+        self.lista_productos = []
+
+    def agregar_Producto(self, nombre, cantidad, precio, ubicacion):
+        producto_nuevo = datos_producto(nombre, cantidad, precio, ubicacion)
+        self.lista_productos.append(producto_nuevo)
+        print("Producto Agregado Correctamente")
+        return True
+
+    def encontrar_producto(self, nombre, ubicacion):
+        for producto in self.lista_productos:
+            if producto.nombre == nombre and producto.ubicacion == ubicacion:
+                return producto
+        return None
+
+    def agregar_stock(self, nombre, ubicacion, cantidad):
+        producto_existente = self.encontrar_producto(nombre, ubicacion)
+        if producto_existente is not None:
+            producto_existente.cantidad += cantidad
+            print("Producto Actualizado Correctamente")
+        else:
+            print("Error: El producto",nombre,"no existe en la ubicación",ubicacion)
+
+    def vender_producto(self, nombre, ubicacion, cantidad):
+        producto_existente = self.encontrar_producto(nombre, ubicacion)
+        if producto_existente is not None:
+            if cantidad < producto_existente.cantidad:
+                producto_existente.cantidad -= cantidad
+                print("Producto Vendido Correctamente")
+            elif cantidad >= producto_existente.cantidad:
+                print("Error: La cantidad a vender de",nombre,"en la ubicación",ubicacion,"es mayor o igual que la existencia.")
+        else:
+            print("Error: El producto",nombre,"no existe en la ubicación",ubicacion)
+
+    def imprimir_productos(self):
+        for producto in self.lista_productos:
+            print(producto)
--- Practica_202201947/app.py
+++ Practica_202201947/app.py
@@ -1,4 +1,72 @@
+from Inventario_DAO import inventario_DAO
+from inventarioClass import datos_producto
+manejador_lista = inventario_DAO()
+
+
+
 a=0
-while a==1:
-    print("buenos dias")
-    a = int(input("Desea Salir 1=si, 0=no"))
+while a!=-1:
+    print("")
+    print("===============================================================")
+    print("  PRACTICA 1 - LENGUAJES FORMALES DE PROGRAMACION - 202201947")
+    print("===============================================================")
+    print("")
+    print("1. Cargar inventario inicial")
+    print("2. Cargar instrucciones de movimientos")
+    print("3. Crear informe de inventario")
+    print("4. Salir")
+    
+    while True:
+        try:
+            a = int(input("Seleccione una opcion: "))
+            if a >= 1 and a <= 4:
+                print("Seleccionaste Opcion:", a)
+                break
+            else:
+                print("Opcion incorrecta, ingrese una opcion válida mayor que 0")
+        except ValueError:
+            print("Opcion incorrecta, ingrese una opción válida (asegúrese que sea un número)")
+
+
+    print("")
+    print("")
+    if a==1:
+        print("")
+        print("Cargara el inventario inicial")
+        
+        #ruta_del_archivo = input("Ingrese la ruta completa del archivo: ")
+        try:
+            with open("Inventario.inv", "r+") as archivo:
+                for linea in archivo:
+                    objeto = linea.strip().replace("crear_producto ", "").split(';')
+                    manejador_lista.agregar_producto(objeto[0], objeto[1], objeto[2], objeto[3])
+            print("--------------------------------------")         
+            manejador_lista.imprimir_productos()
+            print("--------------------------------------")
+        except FileNotFoundError:
+            print("El archivo no existe ")
+        print("--------------------------------------")         
+        print("Continuara al menu con inventario cargado")
+        print("--------------------------------------")
+        print("")
+        print("")
+    elif a==2:
+        print("")
+        print("Cargara las instrucciones de movimientos")
+        print("")
+
+        print("")
+        print("")
+    elif a==3:
+        print("")
+        print("Se creara el informe/reporte de inventario")
+        print("")
+
+        print("")
+        print("")
+    elif a==4:
+        print("")
+        print("ESTA SALIENDO")
+        a=-1
+        break
+
